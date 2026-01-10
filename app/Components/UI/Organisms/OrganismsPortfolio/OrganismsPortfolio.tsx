d30eefad31f697cbee68c085e0a3a286ee651694
RuVerr <ruvermishyan@gmail.com>
2026-01-10 13:24:52 +0400

    Added overflow hidden for one second when navigating to the page

--- app/Components/UI/Organisms/OrganismsPortfolio/OrganismsPortfolio.tsx
+++ app/Components/UI/Organisms/OrganismsPortfolio/OrganismsPortfolio.tsx
@@ -1,12 +1,22 @@
-("");
-import React from "react";
+"use client";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import MoleculesPortfolio from "../../Molecules/MoleculesPortfolio/MoleculesPortfolio";
 import MoleculesBackground from "../../Molecules/MoleculesBackground/MoleculesBackground";
+import HiddenScreen from "@/app/Components/Hooks/HiddenScreen/HiddenScreen";
 
 export default function OrganismsPortfolio() {
+  const [activeHidden, setActiveHidden] = useState<boolean>(true);
+  useLayoutEffect(() => {
+    let hiddenTimeout: ReturnType<typeof setTimeout>;
+
+    hiddenTimeout = setTimeout(() => {
+      setActiveHidden(false);
+    }, 1000);
+  }, []);
   return (
     <>
-      <section className="portfolio w-full h-screen-[100dvh] bg-gray-500 ">
+      <section className="portfolio w-full h-full bg-gray-500 overflow-hidden">
+        <HiddenScreen active={activeHidden} />
         <MoleculesBackground
           className="bg-amber-50 "
           backgroundSRC="/Images-and-video/Background/Video/white-lines.mp4"
