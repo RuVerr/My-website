16ab7d007c385706c64df69c3663da2d4979880a
RuVerr <ruvermishyan@gmail.com>
2026-03-31 00:16:25 +0400

    Update home components now its dynamic

--- app/Components/UI/Molecules/MoleculesLoading/MoleculesLoading.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { AtomLoading } from "../../Atoms/GROUP-AtomBackgrounds/AtomLoading/AtomLoading";
-
-export const MoleculesLoading = () => {
-  const loadingHeading = "Loading";
-  return <AtomLoading children={loadingHeading} className="" spanClassName="" />;
-};
--- app/Components/UI/Templates/TemplateHome/TemplateHome.tsx
+++ app/Components/UI/Templates/TemplateHome/TemplateHome.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import OrganismsHome from "../../Organisms/OrganismsHome/OrganismsHome";
+import dynamic from "next/dynamic";
+import { AtomLoading } from "../../Atoms/GROUP-AtomBackgrounds/AtomLoading/AtomLoading";
 
+const OrganismsHome = dynamic(() => import("@/app/Components/UI/Organisms/OrganismsHome/OrganismsHome"),{loading: () => <AtomLoading className="bg-white" spanClassName="text-black" />});
 export default function TemplateHome() {
   return (
     <>
